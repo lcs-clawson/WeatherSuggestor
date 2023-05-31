be3dab7fba691b94485aa4c06dda147a6ef4d17f
Cooper Lawson <CSLawson@lcs.on.ca>
2023-05-31 12:18:57 -0400

    Sorted my groups, deleted ContentView, and have began to work on my interface.

---
+++ WeatherSuggestor/Views/WeatherSuggestorView.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherSuggestorView.swift
+//  WeatherSuggestor
+//
+//  Created by Cooper Lawson on 2023-05-31.
+//
+
+import SwiftUI
+
+struct WeatherSuggestorView: View {
+    var body: some View {
+        
+        VStack {
+            Text("Weather Suggestor")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("Today's Forecast:")
+            
+            
+        }
+    }
+}
+
+
+
+
+
+struct WeatherSuggestorView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherSuggestorView()
+    }
+}
--- WeatherSuggestor/WeatherSuggestorApp.swift
+++ WeatherSuggestor/WeatherSuggestorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WeatherSuggestorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WeatherSuggestorView()
         }
     }
 }
