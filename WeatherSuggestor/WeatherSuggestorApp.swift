98bda6f515b193fadf1482078ffeea2b3a185db9
Cooper Lawson <CSLawson@lcs.on.ca>
2023-05-31 12:02:11 -0400

    Initial Commit

---
+++ WeatherSuggestor/WeatherSuggestorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSuggestorApp.swift
+//  WeatherSuggestor
+//
+//  Created by Cooper Lawson on 2023-05-31.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSuggestorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
