d52af4ec583ebf970c6d97ed420c2b2a7ad2c174
Cooper Lawson <CSLawson@lcs.on.ca>
2023-06-01 14:32:35 -0400

    Created SuggestionsView

---
+++ WeatherSuggestor/Views/SuggestionsView.swift
@@ -0,0 +1,25 @@
+//
+//  SuggestionsView.swift
+//  WeatherSuggestor
+//
+//  Created by Cooper Lawson on 2023-06-01.
+//
+
+import SwiftUI
+
+struct SuggestionsView: View {
+    var body: some View {
+        
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            
+        }
+    }
+}
+
+struct SuggestionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuggestionsView()
+    }
+}
