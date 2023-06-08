d0bde97f746213018417910c24d99849423ec660
Cooper Lawson <CSLawson@lcs.on.ca>
2023-06-08 13:29:39 -0400

    Used the AppDatabase helper code to establish the database connection and make it available within the app.

--- WeatherSuggestor/WeatherSuggestorApp.swift
+++ WeatherSuggestor/WeatherSuggestorApp.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import Blackbird
 
 @main
 struct WeatherSuggestorApp: App {
     var body: some Scene {
         WindowGroup {
             WeatherSuggestorView()
+                .environment(\.blackbirdDatabase, AppDatabase.instance)
+            // Could use this for the syntax error in the writing ^
         }
     }
 }
