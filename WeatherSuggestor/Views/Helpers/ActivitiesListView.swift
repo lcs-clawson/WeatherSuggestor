ecddfa56dc09556620a5ce1daae3ef71ba8293ef
Cooper Lawson <CSLawson@lcs.on.ca>
2023-06-09 16:49:19 -0400

    Been trying a variety of solutions but xcode keeps crashing.

--- WeatherSuggestor/Views/Helpers/ActivitiesListView.swift
+++ WeatherSuggestor/Views/Helpers/ActivitiesListView.swift
@@ -1,3 +1,4 @@
+
 //
 //  ActivitiesListView.swift
 //  WeatherSuggestor
@@ -9,41 +10,34 @@ import SwiftUI
 import Blackbird
 
 
-
-struct ActivitiesListView: View {
+struct ActivityRow: View {
+    var suggestions: Suggestion
     
-    // MARK Stored Properties
-    
-    @BlackbirdLiveModels ({ Suggestion2db in
-        try await Suggestion.read(from: Suggestion2db)
-    }) var suggestions
-    
-    // MARK Computed Properties
     var body: some View {
-        NavigationView {
-            
-            List(suggestions.results) {currentSuggestion in
-                // Fix activities view then slap it in here
-                ActivitiesView(name: currentSuggestion.name,
-                                highestTemp: currentSuggestion.highestTemp,
-                                lowestTemp: currentSuggestion.lowestTemp)
-
-                
-                
-                //                ActivityRow(activity: Activity(id: 1, name: "Swim", highestTemp: 35, lowestTemp: 15))
-                //                ActivityRow(activity: Activity(id: 2, name: "Go for a walk", highestTemp: 25, lowestTemp: 10))
-                //                ActivityRow(activity: Activity(id: 3, name: "Shoot hoops", highestTemp: 22, lowestTemp: 12))
-            }
-            .navigationTitle("Activities")
-        }
+        Text(suggestions.name)
     }
 }
 
-struct ActivityRow: View {
-    var suggestion: Suggestion
+struct ActivitiesListView: View {
     
+    // MARK: Stored Properties
+    
+    @BlackbirdLiveModels({ Suggestion2db in
+        try await Suggestion.read(from: Suggestion2db)
+    }) var suggestions
+    
+    // MARK: Computed Properties
     var body: some View {
-        Text(suggestion.name)
+        NavigationView {
+            
+            List(suggestions.results) { currentSuggestion in
+                ActivityRow(suggestions: Suggestion(id: currentSuggestion.id,
+                                                    name: currentSuggestion.name,
+                                                    highestTemp: currentSuggestion.highestTemp,
+                                                    lowestTemp: currentSuggestion.lowestTemp))
+            }
+            .navigationTitle("Activities")
+        }
     }
 }
 
@@ -54,3 +48,4 @@ struct ActivitiesListView_Previews: PreviewProvider {
     }
 }
 // Suggestions: Suggestion(id: 1, name: "Swim", highestTemp: 35, lowestTemp: 20
+
--- WeatherSuggestor/Views/Model/Activity.swift
+++ WeatherSuggestor/Views/Model/Activity.swift
@@ -15,8 +15,8 @@ import Blackbird
 //    @BlackbirdColumn var lowestTemp: Int
 //}
 
-let name = "Swim"
-let highestTemp = 35
-let lowestTemp = 20
+//let name = "Swim"
+//let highestTemp = 35
+//let lowestTemp = 20
 
-let currentSuggestion = Suggestion(id: 1, name: "Swim", highestTemp: 35, lowestTemp: 20)
+//let currentSuggestion = Suggestion(id: 1, name: "Swim", highestTemp: 35, lowestTemp: 20)
